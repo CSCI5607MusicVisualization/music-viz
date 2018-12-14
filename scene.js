900fe49e049d8afc1a5d75208d351eba71b156e7
Carter Chan <chan0905@umn.edu>
2018-12-14 14:57:56 -0600

    10 trees in a row; more ambient light

--- scene.js
+++ scene.js
@@ -43,52 +43,61 @@ function drawPlace(){
   // To add multiple lights append to and of array of light locations
   mat4.multiplyVec3( app.mvMatrix, app.lightLocationStatic[0], app.lightLocation )
   // console.log(app.lightLocationStatic);
-  console.log(app.lightLocation);  
+  // console.log(app.lightLocation);  
   gl.uniform3fv( shaderProgram.lightLocation, app.lightLocation );
   gl.uniform3fv( shaderProgram.lightVector, app.lightVector );
   gl.uniform1i( shaderProgram.lightCount, 2);
   
   setUniforms();
   
+  // Root transform start
+  // ===============================================
   mvPushMatrix();
+
     mat4.scale( app.mvMatrix, [2,2,2] )
-    // THIS IS A SINGLE OBJECT
-    // drawObject( app.models.room_walls, 0 );
-    // if( !app.breakWalls ) {
-      // drawObject( app.models.room_wall_unbroken, 0 );
-    // }
-    // drawObject( app.models.room_floor, 0 );
-    // drawObject( app.models.room_ceiling, 0 );
-    drawObject( app.models.pedestal, 50, [0.75,0.75,0.75,1.0] );
-      
+    
+    // Draw floor
+    drawObject( app.models.room_floor, 0 );
+
+      // Push the model matrix for trees
       mvPushMatrix();
 
-
-        mat4.scale( app.mvMatrix, [0.15,0.05,0.05] )
+        // General world transformations:
+        mat4.scale( app.mvMatrix, [0.15, 0.05, 0.05] )
         mat4.rotate( app.mvMatrix, degToRad( 180 ), [0,1,0] );
-        mat4.translate( app.mvMatrix,  [0, 1, 0] );        
-        // mvPushMatrix();        
-        for (let i = 0; i < 10; i++) {
-          mat4.translate( app.mvMatrix, app.monkey.position);
-          mat4.translate( app.mvMatrix,  [0 + .6, 0, 0] );                  
-          drawObject( app.models.suzanne, 0, [1, 1, 0, 1] );         
+        // mat4.translate( app.mvMatrix, [0, 1, 0] );    
+        
+        for (let i = 0; i < 10; i++)
+        // Push 10 trees 
+        {
+
+            // Move tree to center of screen
+            // mat4.translate( app.mvMatrix, app.monkey.position);
+
+            // Move tree apart from nearby others by space of size `tree_space`
+            tree_space = 2;
+            mat4.translate( app.mvMatrix,  [2 * tree_space, 0, 2 * tree_space] );
+
+            // Actually draw tree
+            drawObject( app.models.tree, 0, [1, 1, 0, 1] );
+
         }
-        // mvPopMatrix();        
-        mat4.translate( app.mvMatrix, app.monkey.position );
-        mat4.translate( app.mvMatrix,  [0, 2.5, 0] );
-        drawObject( app.models.tree01, 0, [1, 1, 0, 1] );
-      mvPopMatrix();
+        mvPopMatrix();
     
 
       mvPushMatrix();
         mat4.translate( app.mvMatrix, [0,2,0] );
         gl.uniform3fv( shaderProgram.ambientColorUniform, lightIntesity( 2.0, 1,1,1 ) );
-        drawObject( app.models.skylight, 0, [0.53,0.81,0.98,1.0] );
+        drawObject( app.models.skylight, 0, [0.53, 0.81, 0.98, 1.0] );
         gl.uniform3fv( shaderProgram.ambientColorUniform, lightIntesity( app.ambientIntensity, 0.3,0.3,0.3 ) );
       mvPopMatrix();
     
-    drawObject( app.models.room_tunnel_ceiling, 0 );
-    drawObject( app.models.room_tunnel_walls, 0 );
+    // Old tunnel stuff
+    // drawObject( app.models.room_tunnel_ceiling, 0 );
+    // drawObject( app.models.room_tunnel_walls, 0 );
+
+  // Root transform start
+  // ===============================================
   mvPopMatrix();
 }
 
